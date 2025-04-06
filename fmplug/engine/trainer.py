3a2de4c57c167cafd4b738fb7ae176cdcaae30eb
Ryan Devera <dever120@umn.edu>
2025-04-06 13:18:23 -0500

    adding training epoch

---
+++ fmplug/engine/trainer.py
@@ -0,0 +1,109 @@
+# stdlib
+import gc
+import logging
+from typing import Iterable
+
+# third party
+import torch
+from flow_matching.path import CondOTProbPath
+from torch.nn.parallel import DistributedDataParallel
+from torchmetrics.aggregation import MeanMetric
+
+# first party
+from fmplug.engine.scaling import NativeScaler
+from fmplug.engine.utils import skewed_timestep_sample
+from fmplug.layers.ema import EMA
+
+# global variable
+PRINT_FREQUENCY = 50
+
+
+# Create logger
+logger = logging.getLogger(__name__)
+
+
+def train_one_epoch(
+    model: torch.nn.Module,
+    data_loader: Iterable,
+    optimizer: torch.optim.Optimizer,
+    lr_schedule: torch.torch.optim.lr_scheduler.LRScheduler,
+    device: torch.device,
+    epoch: int,
+    loss_scaler: NativeScaler,
+):
+    gc.collect()
+    model.train(True)
+    batch_loss = MeanMetric().to(device, non_blocking=True)
+    epoch_loss = MeanMetric().to(device, non_blocking=True)
+
+    accum_iter = 1
+    loss_violation = 0
+    path = CondOTProbPath()
+
+    for data_iter_step, (samples, labels) in enumerate(data_loader):
+        if data_iter_step % accum_iter == 0:
+            optimizer.zero_grad()
+            batch_loss.reset()
+
+        samples = samples.to(device, non_blocking=True)
+        labels = labels.to(device, non_blocking=True)
+
+        if torch.rand(1) < 1.0:
+            conditioning = {}
+        else:
+            conditioning = {"label": labels}
+
+        # Scaling to [-1, 1] from [0, 1]
+        samples = samples * 2.0 - 1.0
+        noise = torch.randn_like(samples).to(device)
+
+        if True:
+            t = skewed_timestep_sample(samples.shape[0], device=device)
+        else:
+            t = torch.torch.rand(samples.shape[0]).to(device)
+
+        path_sample = path.sample(t=t, x_0=noise, x_1=samples)
+        x_t = path_sample.x_t
+        u_t = path_sample.dx_t
+
+        with torch.cuda.amp.autocast(enabled=True, dtype=torch.float32):
+            model_output = model(x_t, t, extra=conditioning)
+            diff = model_output - u_t
+            loss = torch.pow(diff, 2).mean()
+
+        batch_loss.update(loss)
+        epoch_loss.update(loss)
+
+        loss /= accum_iter
+
+        # Loss scaler applies the optimizer when update_grad is set to true.
+        # Otherwise just updates the internal gradient scales
+        apply_update = (data_iter_step + 1) % accum_iter == 0
+        loss_scaler(
+            loss,
+            optimizer,
+            parameters=model.parameters(),
+            update_grad=apply_update,
+            clip_grad=2.5,
+        )
+
+        if apply_update and isinstance(model, EMA):
+            model.update_ema()
+
+        elif (
+            apply_update
+            and isinstance(model, DistributedDataParallel)
+            and isinstance(model.module, EMA)
+        ):
+            model.module.update_ema()
+
+        lr = optimizer.param_groups[0]["lr"]
+        if data_iter_step % PRINT_FREQUENCY == 0:
+            logger.info(
+                f"Epoch {epoch} [{data_iter_step}/{len(data_loader)}]:"  # type: ignore
+                f"loss = {batch_loss.compute()}, lr = {lr}"
+            )
+
+    lr_schedule.step()
+    print(f"Number of loss violations: {loss_violation}")
+    return {"loss": float(epoch_loss.compute().detach().cpu())}
---
+++ fmplug/models/cfg_scaled.py
@@ -0,0 +1,62 @@
+# third party
+import torch
+import torch.nn as nn
+from flow_matching.utils import ModelWrapper
+
+
+class CFGScaledModel(ModelWrapper):
+    def __init__(self, model: nn.Module):
+        super().__init__(model)
+        self.nfe_counter = 0
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        t: torch.Tensor,
+        cfg_scale: float,
+        label: torch.Tensor,
+        is_discrete: bool = False,
+    ):
+        assert cfg_scale == 0.0 or not is_discrete, (
+            "Cfg scaling does not work for the logit outputs "
+            f"of discrete models. Got cfg weight={cfg_scale} and "
+            f"model {type(self.model)}."
+        )
+
+        t = torch.zeros(x.shape[0], device=x.device) + t
+
+        if cfg_scale != 0.0:
+            with (
+                torch.cuda.amp.autocast(enabled=True, dtype=torch.float32),
+                torch.no_grad(),
+            ):
+                conditional = self.model(x, t, extra={"label": label})
+                condition_free = self.model(x, t, extra={})
+
+            result = (1.0 + cfg_scale) * conditional - cfg_scale * condition_free
+            # cond + cfg * cond - cfg * uncond
+            #
+        else:
+            # Model is fully conditional, no cfg weighting needed
+            with (
+                torch.cuda.amp.autocast(enabled=True, dtype=torch.float32),
+                torch.no_grad(),
+            ):
+                result = self.model(
+                    x,
+                    t,
+                    extra={},
+                )
+
+        # NOTE: autocast here is float32 because there was bad
+        # under and overflow with float16
+
+        self.nfe_counter += 1
+
+        return result.to(dtype=torch.float32)
+
+    def reset_nfe_counter(self) -> None:
+        self.nfe_counter = 0
+
+    def get_nfe(self) -> int:
+        return self.nfe_counter
