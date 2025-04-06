38d994453cbdc7019a05a5509e188052ce0baca3
Ryan Devera <dever120@umn.edu>
2025-04-06 12:40:38 -0500

    adding utils and attention layers

--- fmplug/layers/utils.py
+++ fmplug/layers/utils.py
@@ -2,6 +2,7 @@
 import math
 
 # third party
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -139,3 +140,23 @@ def checkpoint(func, inputs, params, flag):
 
     else:
         return func(*inputs)
+
+
+def count_flops_attn(model, _x, y):
+    """
+    A counter for the `thop` package to count the operations in an
+    attention operation.
+    Meant to be used like:
+        macs, params = thop.profile(
+            model,
+            inputs=(inputs, timestamps),
+            custom_ops={QKVAttention: QKVAttention.count_flops},
+        )
+    """
+    b, c, *spatial = y[0].shape
+    num_spatial = int(np.prod(spatial))
+    # We perform two matmuls with the same number of ops.
+    # The first computes the weight matrix, the second computes
+    # the combination of the value vectors.
+    matmul_ops = 2 * b * (num_spatial**2) * c
+    model.total_ops += torch.DoubleTensor([matmul_ops])
